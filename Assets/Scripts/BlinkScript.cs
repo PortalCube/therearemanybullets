caa426d87e16277a51cd49cddac26fd4e1e2b228
PortalCube <35104213+PortalCube@users.noreply.github.com>
2020-11-25 16:05:29 +0900

    add: BlinkScript 추가

---
+++ Assets/Scripts/BlinkScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkScript : MonoBehaviour
+{
+    public float fireTime = 1f;
+    public float sleepTime = 0f;
+    public float offset = 0;
+
+    private bool isActive = false;
+    private float time = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void On(List<float> args) {
+        fireTime = args[0];
+        sleepTime = args[1];
+        offset = args[2];
+        time = offset;
+        isActive = true;
+    }
+
+    public void Off() {
+        isActive = false;
+        gameObject.GetComponent<SpawnerScript>().SetStatus(true);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isActive) {
+            time += Time.deltaTime;
+
+            if (time < fireTime) {
+                gameObject.GetComponent<SpawnerScript>().SetStatus(true);
+            } else if (time < fireTime + sleepTime) {
+                gameObject.GetComponent<SpawnerScript>().SetStatus(false);
+            } else {
+                time = 0;
+            }
+        }
+    }
+}
