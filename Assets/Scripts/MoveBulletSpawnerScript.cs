a7a588aadc3811237c373e1cb70b757d0271b985
PortalCube <35104213+PortalCube@users.noreply.github.com>
2020-11-26 19:02:28 +0900

    add: MoveBulletSpawnerScript 추가

---
+++ Assets/Scripts/MoveBulletSpawnerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBulletSpawnerScript : MonoBehaviour, IBulletSpawner {
+
+    public float speed = 3f;
+    public float direction;
+
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public float GetSpeed() {
+        return speed;
+    }
+
+    public float GetDirection() {
+        return direction;
+    }
+
+    public void Init(List<float> args) { }
+
+    public void Fire() {
+        Vector3 vector = transform.localPosition;
+        direction = transform.localRotation.eulerAngles.z;
+
+        GameObject bulletObject = Instantiate(bullet, vector, Quaternion.identity);
+
+        var bulletScript = bulletObject.GetComponent<MovingBulletScript>();
+
+        bulletScript.direction = GetDirection();
+        bulletScript.speed = GetSpeed();
+        bulletScript.lockToParent = true;
+        bulletScript.spawner = this;
+    }
+
+}
--- Assets/Scripts/MovingBulletScript.cs
+++ Assets/Scripts/MovingBulletScript.cs
@@ -5,6 +5,9 @@ public class MovingBulletScript : MonoBehaviour {
     public float direction;
     public float speed;
 
+    public bool lockToParent = false;
+    public IBulletSpawner spawner;
+
     // Start is called before the first frame update
     void Start() {
 
@@ -14,6 +17,11 @@ public class MovingBulletScript : MonoBehaviour {
     void Update() {
         if (GameManager.instance.gameActive) {
             Vector3 vector = new Vector3(speed * Time.deltaTime, 0, 0);
+
+            if (lockToParent) {
+                direction = spawner.GetDirection();
+                speed = spawner.GetSpeed();
+            }
 
             // 각도 설정
             transform.localRotation = Quaternion.Euler(0, 0, direction);
